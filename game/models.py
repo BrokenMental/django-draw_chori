fb3804c5ecdd30e8ccf32cd8143222573a43fcd7
jinuk-home <commontoday@naver.com>
2024-09-30 16:27:16 +0900

    이미지 업로드, 선택, 업로드 페이지 비밀번호 입력, 비교 기능 추가(테스트 아직 안함)

--- game/models.py
+++ game/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class ReferenceImage(models.Model):
+    title = models.CharField(max_length=100)  # 이미지 제목 필드
+    image = models.ImageField(upload_to='reference_images/')  # 이미지를 업로드할 경로 설정
+
+    def __str__(self):
+        return self.title
--- game/urls.py
+++ game/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('draw/', views.draw_view, name='draw'),
+    path('upload/', views.upload_image_view, name='upload_image'),
+    path('password/', views.password_view, name='password_page'),
+    path('select-image/', views.select_image_view, name='select_image'),
 ]
